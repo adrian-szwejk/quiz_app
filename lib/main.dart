d8ceb1a09fe245b74977cfaa61336ef9f1bd236a
adrian-szwejk <szwejkowski.adrian@gmail.com>
2022-08-09 00:02:58 -0500

    added cases for hovering/focusing/pressing button

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int _counter = 0;
-  int q_index = 0;
+  int _index = 0;
   final Map<String, int> questions_ = {
     "What is the color of the sun": 1,
     "How many feet are in a mile": 2,
@@ -46,12 +46,12 @@ class _HomePageState extends State<HomePage> {
   void _incrementCounter() {
     setState(() {
       _counter++;
-      q_index++;
+      //_index++;
     });
   }
 
   bool _checkAnswer(int key) {
-    if (questions_.values.elementAt(q_index) == key) {
+    if (questions_.values.elementAt(_index) == key) {
       return true;
     }
     return false;
@@ -71,7 +71,7 @@ class _HomePageState extends State<HomePage> {
             Align(
               alignment: Alignment.topRight,
               child: Text(
-                'Correct: $_counter Idx: $q_index',
+                'Correct: $_counter Idx: $_index',
                 style: const TextStyle(
                   fontSize: 20.0,
                 ),
@@ -86,14 +86,14 @@ class _HomePageState extends State<HomePage> {
                       padding: const EdgeInsets.fromLTRB(15.0, 0.0, 15.0, 10.0),
                       child: Text(
                         textAlign: TextAlign.center,
-                        "${questions_.keys.elementAt(q_index)}?",
+                        "${questions_.keys.elementAt(_index)}?",
                         style: const TextStyle(
                           fontSize: 30.0,
                         ),
                       ),
                     ),
                     buildButton(
-                      text: answers_.values.elementAt(q_index).elementAt(0),
+                      text: answers_.values.elementAt(_index).elementAt(0),
                       color: const Color.fromARGB(255, 205, 29, 26),
                       fontColor: Colors.black,
                       fontSize: 20.0,
@@ -101,7 +101,7 @@ class _HomePageState extends State<HomePage> {
                       key: "0",
                     ),
                     buildButton(
-                      text: answers_.values.elementAt(q_index).elementAt(1),
+                      text: answers_.values.elementAt(_index).elementAt(1),
                       color: const Color.fromARGB(255, 36, 202, 236),
                       fontColor: Colors.black,
                       fontSize: 20.0,
@@ -109,7 +109,7 @@ class _HomePageState extends State<HomePage> {
                       key: "1",
                     ),
                     buildButton(
-                      text: answers_.values.elementAt(q_index).elementAt(2),
+                      text: answers_.values.elementAt(_index).elementAt(2),
                       color: const Color.fromARGB(255, 225, 62, 193),
                       fontColor: Colors.black,
                       fontSize: 20.0,
@@ -117,7 +117,7 @@ class _HomePageState extends State<HomePage> {
                       key: "2",
                     ),
                     buildButton(
-                      text: answers_.values.elementAt(q_index).elementAt(3),
+                      text: answers_.values.elementAt(_index).elementAt(3),
                       color: const Color.fromARGB(255, 140, 242, 15),
                       fontColor: Colors.black,
                       fontSize: 20.0,
@@ -142,9 +142,9 @@ class _HomePageState extends State<HomePage> {
 
 Container buildButton({
   String text = "",
-  Color fontColor = Colors.green,
+  Color fontColor = Colors.black,
   double fontSize = 30.0,
-  Color? color,
+  Color color = Colors.grey,
   VoidCallback? function,
   String key = "1",
 }) {
@@ -152,9 +152,24 @@ Container buildButton({
     padding: const EdgeInsets.fromLTRB(10.0, 30.0, 10.0, 30.0),
     child: TextButton(
       key: Key(key),
-      onPressed: function,
+      onPressed: () {
+        function;
+      },
       style: ButtonStyle(
-        backgroundColor: MaterialStateProperty.all(color),
+        backgroundColor: MaterialStateProperty.resolveWith<Color>(
+            (Set<MaterialState> states) {
+          if (states.contains(MaterialState.focused) && function != null) {
+            return Colors.green;
+          }
+          if (states.contains(MaterialState.pressed) && function != null) {
+            return Colors.red;
+          }
+          if (states.contains(MaterialState.hovered) && function != null) {
+            return Colors.blue;
+          }
+          return color;
+        }),
+        //backgroundColor: MaterialStateProperty.all(color),
         elevation: MaterialStateProperty.all(3.0),
         padding: MaterialStateProperty.all(const EdgeInsets.all(10.0)),
         fixedSize: MaterialStateProperty.all(const Size.fromWidth(350)),
