b19901fb67e375f4210606b35a7819f08a556161
adrian-szwejk <szwejkowski.adrian@gmail.com>
2022-08-11 13:58:20 -0500

    created stack to nav bottom nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -28,115 +28,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  //Navigation bar options
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Home',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: Business',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: School',
-      style: optionStyle,
-    ),
-  ];
-
-  //Other screens
-  final Widget _myHome = MyHome();
-  final Widget _myProfile = MyProfile();
-  final Widget _myLearn = MyLearn();
-
-  //Other variables
-
-  int _nav_idx = 0;
-  
-
-  @override
-  Widget build(BuildContext context) {
-    //fillQNA();
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        centerTitle: true,
-      ),
-      body: Center (
-        child: _widgetOptions.elementAt(_nav_idx),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        currentIndex: _nav_idx,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'Learn',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.face),
-            label: 'Profile',
-          ),
-        ],
-        onTap: (int index) {
-          this.onTapHandler(index);
-        },
-      ),
-      // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-
-  //Helper functions for bottom nav bar
-  Widget getBody() {
-    if (_nav_idx == 0) {
-      return this._myHome;
-    } else if (_nav_idx == 1) {
-      return this._myLearn;
-    } else {
-      return this._myProfile;
-    }
-  }
-
-  void onTapHandler(int index) {
-    setState(() {
-      _nav_idx = index;
-    });
-  }
-}
-
-//Classes for each screen on bottom nav bar
-class MyProfile extends StatelessWidget {
-  const MyProfile({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Text("My Profile"),
-    );
-  }
-}
-
-class MyHome extends StatelessWidget {
-  const MyHome({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Home screen"),
-    );
-  }
-}
-
-class MyLearn extends State<HomePage> {
-
+  //Variables
   int _counter = 0;
   int _index = 0;
+  int _navIdx = 0;
 
   final Map<String, int> questions_ = {
     "What is the color of the sun": 1,
@@ -165,74 +60,130 @@ class MyLearn extends State<HomePage> {
     return false;
   }
 
+  //Navigation bar options
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+
   @override
   Widget build(BuildContext context) {
-    return  Center(
-        child: Column(
-          children: <Widget>[
-            Align(
-              alignment: Alignment.topRight,
-              child: Text(
-                'Correct: $_counter Idx: $_index',
-                style: const TextStyle(
-                  fontSize: 20.0,
-                ),
-              ),
-            ),
-            Expanded(
-              child: Align(
-                child: Column(
-                  children: <Widget>[
-                    Padding(
-                      padding:
-                          const EdgeInsets.fromLTRB(15.0, 50.0, 15.0, 10.0),
-                      child: Text(
-                        textAlign: TextAlign.center,
-                        "${questions_.keys.elementAt(_index)}?",
-                        style: const TextStyle(
-                          fontSize: 30.0,
-                        ),
-                      ),
-                    ),
-                    buildButton(
-                      text: answers_.values.elementAt(_index).elementAt(0),
-                      color: const Color.fromARGB(255, 205, 29, 26),
-                      fontColor: Colors.black,
-                      fontSize: 20.0,
-                      function: _checkAnswer(0) ? _incrementCounter : () {},
-                      key: "0",
-                    ),
-                    buildButton(
-                      text: answers_.values.elementAt(_index).elementAt(1),
-                      color: const Color.fromARGB(255, 36, 202, 236),
-                      fontColor: Colors.black,
-                      fontSize: 20.0,
-                      function: _checkAnswer(1) ? _incrementCounter : () {},
-                      key: "1",
-                    ),
-                    buildButton(
-                      text: answers_.values.elementAt(_index).elementAt(2),
-                      color: const Color.fromARGB(255, 225, 62, 193),
-                      fontColor: Colors.black,
-                      fontSize: 20.0,
-                      function: _checkAnswer(2) ? _incrementCounter : () {},
-                      key: "2",
-                    ),
-                    buildButton(
-                      text: answers_.values.elementAt(_index).elementAt(3),
-                      color: const Color.fromARGB(255, 140, 242, 15),
-                      fontColor: Colors.black,
-                      fontSize: 20.0,
-                      function: _checkAnswer(3) ? _incrementCounter : () {},
-                      key: "3",
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
+    //fillQNA();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true,
       ),
+      body: IndexedStack(
+        index: _navIdx,
+        children: <Widget>[
+          //Stack
+          //Home
+          const Text(
+            'Home',
+            style: optionStyle,
+          ),
+          //Learn
+          Center(
+            child: Column(
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.topRight,
+                  child: Text(
+                    'Correct: $_counter Idx: $_index',
+                    style: const TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Align(
+                    child: Column(
+                      children: <Widget>[
+                        Padding(
+                          padding:
+                              const EdgeInsets.fromLTRB(15.0, 50.0, 15.0, 10.0),
+                          child: Text(
+                            textAlign: TextAlign.center,
+                            "${questions_.keys.elementAt(_index)}?",
+                            style: const TextStyle(
+                              fontSize: 30.0,
+                            ),
+                          ),
+                        ),
+                        buildButton(
+                          text: answers_.values.elementAt(_index).elementAt(0),
+                          color: const Color.fromARGB(255, 205, 29, 26),
+                          fontColor: Colors.black,
+                          fontSize: 20.0,
+                          function: _checkAnswer(0) ? _incrementCounter : () {},
+                          key: "0",
+                        ),
+                        buildButton(
+                          text: answers_.values.elementAt(_index).elementAt(1),
+                          color: const Color.fromARGB(255, 36, 202, 236),
+                          fontColor: Colors.black,
+                          fontSize: 20.0,
+                          function: _checkAnswer(1) ? _incrementCounter : () {},
+                          key: "1",
+                        ),
+                        buildButton(
+                          text: answers_.values.elementAt(_index).elementAt(2),
+                          color: const Color.fromARGB(255, 225, 62, 193),
+                          fontColor: Colors.black,
+                          fontSize: 20.0,
+                          function: _checkAnswer(2) ? _incrementCounter : () {},
+                          key: "2",
+                        ),
+                        buildButton(
+                          text: answers_.values.elementAt(_index).elementAt(3),
+                          color: const Color.fromARGB(255, 140, 242, 15),
+                          fontColor: Colors.black,
+                          fontSize: 20.0,
+                          function: _checkAnswer(3) ? _incrementCounter : () {},
+                          key: "3",
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          //Home
+          const Text(
+            'Profile',
+            style: optionStyle,
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _navIdx,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'Learn',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.face),
+            label: 'Profile',
+          ),
+        ],
+        onTap: (int index) {
+          onTapHandler(index);
+        },
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  void onTapHandler(int index) {
+    setState(() {
+      _navIdx = index;
+    });
   }
 }
 
